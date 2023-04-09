b2251a705703ad797ddbf05a405678f0283880d7
DmitryRedko <dar27@tpu.ru>
2023-04-09 17:02:12 +0700

    done

--- ЛБ3/task.py
+++ ЛБ3/task.py
@@ -8,27 +8,23 @@ line, = ax.plot([])
 
 W = [1, 2, 3, 4, 5]
 
-
 def f():
     N = 4002
     W = [[-1, 1][random.randrange(2)] for i in range(N)]
-    Res = [sum(W[:i])/len(W[:i]) for i in range(1,len(W))]
+    Res = [sum(W[:i])/sqrt(N) for i in range(1, len(W))]
 
     return Res
 
-
 X = [i/4000 for i in range(4001)]
 W = f()
-
-colors = ['red', 'green', 'purple', 'orange', 'pink']
-colors1 = ['purple', 'orange', 'pink', 'red', 'green']
 
 for i in range(len(W)):
     line.set_data(range(i+1), W[:i+1])
     ax.set_xlim(0, len(W))
     ax.set_ylim(min(W), max(W))
-    ax.set_facecolor(colors1[i % 5])
     line.set_linewidth(0.5)
-    line.set_color(colors[i % 5])
+    line.set_color("Red")
     plt.draw()
-    plt.pause(10**(-20))
+    plt.pause(1000**(-100))
+
+
